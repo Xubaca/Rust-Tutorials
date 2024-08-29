14f86a5faa37f35a2be55d5df891a882b86d65fb
Xubaca <101934683+Xubaca@users.noreply.github.com>
2024-08-29 03:28:09 +0100

    Chapter 3
    I jumped chapter 2 because it was mostly just the guessing game

---
+++ 003/3.1/variables/target/debug/deps/variables-d43dd46a1dc2e6f7.d
@@ -0,0 +1,5 @@
+/home/xubaca/Rust/003/3.1/variables/target/debug/deps/libvariables-d43dd46a1dc2e6f7.rmeta: src/main.rs
+
+/home/xubaca/Rust/003/3.1/variables/target/debug/deps/variables-d43dd46a1dc2e6f7.d: src/main.rs
+
+src/main.rs:
---
+++ 003/3.1/variables/target/debug/deps/variables-f4b709060a6f8104.d
@@ -0,0 +1,5 @@
+/home/xubaca/Rust/003/3.1/variables/target/debug/deps/variables-f4b709060a6f8104: src/main.rs
+
+/home/xubaca/Rust/003/3.1/variables/target/debug/deps/variables-f4b709060a6f8104.d: src/main.rs
+
+src/main.rs:
